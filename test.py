4c9c3947e5dc99098da41d7af088528b73b3ef6a
BrunoDevels <99835817+BrunoDevels@users.noreply.github.com>
2022-02-18 12:12:30 -0300

    teste

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 print ('Hello World')
-=======
-print ('Hello World') 
->>>>>>> 5aee77aab8696495f2a8b9ab01e36b07d95094f9
-=======
-print ('Hello World') 
->>>>>>> 5aee77aab8696495f2a8b9ab01e36b07d95094f9
